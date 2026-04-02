10038bfc4c88ab375243bb7477efc2a83e5564bc
ecetinerdem <cetoerdem1@gmail.com>
2026-04-02 21:48:05 +0400

    get model formula and load model

--- housing_analysis/model.py
+++ housing_analysis/model.py
@@ -65,3 +65,88 @@ def evaluate_model(data: ModelData, model: LinearRegression, scaler: StandardSca
         train_rmse=train_rmse,
         test_rmse=test_rmse
     )
+
+
+def save_model(model_results: ModelResults, model_path: str, meatadata_path: str) -> None:
+    try:
+        # Create directory if not exist
+        model_dir = os.path.dirname(model_path)
+        if model_dir and not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        metadata_dir = os.path.dirname(meatadata_path)
+        if metadata_dir and not os.path.exists(metadata_dir):
+            os.makedirs(metadata_dir)
+        
+        # Save model and scaler
+        with open(model_path, "wb") as f:
+            model_components = {
+                "model": model_results.model,
+                "scaler": model_results.scaler,
+            }
+
+            pickle.dump(model_components, f)
+
+        intercept, coefficients = get_model_formula(model_results)
+
+        metadata = {
+            "intercept": intercept,
+            "coeffecient": coefficients,
+            "feature_names": CONFIG["feature_columns"],
+            "target_name": CONFIG["target_columns"],
+            "train_r2": float(model_results.train_r2),
+            "test_r2": float(model_results.test_r2),
+            "train_rmse": float(model_results.train_rmse),
+            "test_rmse": float(model_results.test_rmse),
+        }
+        with open(meatadata_path, "r") as f:
+            json.dump(metadata, f, indent=4)
+
+        logger.info(f"Model saved to {model_path}")
+        logger.info(f"Metadata saved to {meatadata_path}")
+    except Exception as e:
+        error_msg = f"Error saving model {str(e)}"
+        logger.error(error_msg)
+        raise ModelOperation(error_msg)
+    
+
+def get_model_formula(model_results: ModelResults) -> Tuple[float, List[float]]:
+    model = model_results.model
+    scaler = model_results.scaler
+
+    coeffecients = []
+    for i in range(len(model.coef_)):
+        coef = model.coef_[i] / scaler.scale_[i]
+        coeffecients.append(coef)
+    
+    intercept = model.intercept_ - sum(
+        model.coef_[i] * scaler.mean_[i] / scaler.scale_[i]
+        for i in range(len(model.coef_))
+    )
+
+    return intercept, coeffecients
+
+
+def load_model(model_path: str, metadata_path: str = None) -> Tuple[LinearRegression, StandardScaler, Dict[str, Any]]:
+    try:
+        if not os.path.isfile(model_path):
+            error_msg = f"Model file does not exist {model_path}"
+            logger.error(error_msg)
+            raise ModelOperation(error_msg)
+        
+        with open(model_path, "rb") as f:
+            model_components = pickle.load(f)
+            model = model_components["model"]
+            scaler = model.components["scaler"]
+
+        if metadata_path and not os.path.isfile(metadata_path):
+            with open(metadata_path, "r") as f:
+                metadata = json.load(metadata_path)
+
+        logger.info(f"Model loaded from: {model_path}")
+        if metadata:
+            logger.info(f"Metadata loaded from: {metadata_path}")
+    except Exception as e:
+        error_msg = f"Error loading model: {str(e)}"
+        logger.error(error_msg)
+        raise ModelOperation(error_msg)
+    return model, scaler, metadata
